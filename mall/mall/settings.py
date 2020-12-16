c5e402fcce0ebd5c12283af8af5960eea1940d74
maoqiansheng <980054594@qq.com>
2020-12-16 23:57:51 +0800

    数据库配置

--- mall/mall/settings.py
+++ mall/mall/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',  # 数据库主机
+        'PORT': 3306,  # 数据库端口
+        'USER': 'maoqiansheng',  # 数据库用户名
+        'PASSWORD': 'mao1314520',  # 数据库用户密码
+        'NAME': 'mall'  # 数据库名字
     }
 }
 
